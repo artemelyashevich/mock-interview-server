f32cb6b96b4ea65f1d7d0af754e16c6ccf52916a
artemelyashevich <elyashevich.artem06@gmail.com>
2025-08-09 17:41:27 +0300

    Add: security logic

---
+++ auth/src/main/java/com/mock/interview/auth/infrastructure/configuration/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.mock.interview.auth.infrastructure.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+            .authorizeHttpRequests(auth ->
+                    auth
+                            .requestMatchers("/api/v1/auth/**").permitAll()
+                            .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth2Login -> {
+              oauth2Login.successHandler(
+                      (request, response, authentication) -> {
+// TODO
+                      }
+              );
+            })
+            .build();
+  }
+}
